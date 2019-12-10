8fd7daaa464da06d8aaead3d96cc90c81c7af15e
Tomas Ortiz <TomasOrtiz@protonmail.com>
2019-12-10 12:17:40 -0800

    exported the arrays only

--- src/data.js
+++ src/data.js
@@ -30,3 +30,33 @@ const operators = [
 ];
 
 const specials = ["C", "+/-", "%"];
+
+
+// exporting arrays here ↓↓↓
+
+export const numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "."];
+
+export const operators = [
+  {
+    char: "/",
+    value: "/"
+  },
+  {
+    char: "x",
+    value: "*"
+  },
+  {
+    char: "-",
+    value: "-"
+  },
+  {
+    char: "+",
+    value: "+"
+  },
+  {
+    char: "=",
+    value: "="
+  }
+];
+
+export const specials = ["C", "+/-", "%"];
